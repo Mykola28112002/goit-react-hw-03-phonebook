9e6c908e2ba924e9f76ef4006db3a239a4244060
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-08-20 12:38:49 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,7 +53,18 @@ export class App extends Component {
       filter: '',
     }));
   };
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts)
+    this.setState({ contacts: parsedContacts });
+  };
 
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    };
+  };
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
